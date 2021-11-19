9a98be0c2ee5009be69345ce5af6aaab7b1997b1
GIOVANNA BOLDRIN <giovanna.boldrin@senacsp.edu.br>
2021-11-18 21:29:29 -0300

    .

--- ProdutosLimpeza/AddCustumerWindow.xaml.cs
+++ ProdutosLimpeza/AddCustumerWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,18 +16,36 @@ using System.Windows.Shapes;
 
 namespace ProdutosLimpeza
 {
+    
     /// <summary>
     /// Lógica interna para addCustumerWindow.xaml
     /// </summary>
     public partial class addCustumerWindow : Window
     {
+        string connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\dados\\NORTHWND.MDF;Integrated Security=True; Connect Timeout=30;User Instance=True";
+
         public addCustumerWindow()
         {
             InitializeComponent();
         }
 
+       
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            using(var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                var cmd = $"INSERT INTO Enderco (Cidade, Bairro, Rua, Numero, Complemento) VALUES ( '{customerCidadeTextBox.Text}', '{customerBairroTextBox.Text}'";
+                var sqlCommand = new SqlCommand(cmd, conn);
+                var result = sqlCommand.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Dados Inseridos com Sucesso");
+                }
+            }
 
         }
     }
