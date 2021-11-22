f4ab1193cd8c211b0a8e9eab82fb98a555b4e360
LUCAS OLIVEIRA DOS SANTOS <lucas.osantos42@senacsp.edu.br>
2021-11-22 18:49:05 -0300

    a

--- ProdutosLimpeza/Pedidos.xaml.cs
+++ ProdutosLimpeza/Pedidos.xaml.cs
@@ -93,7 +93,7 @@ namespace ProdutosLimpeza
         {
            var conn = new SqlConnection(connectionString);
             conn.Open();
-            string cmd = $"INSERT INTO ProdutosFaturados (iD, Produto, Quantidade, FPagamento) VALUE ( '{pedidoCombo.Text}', '{preco1.Text}', '{pagamentoCombo}')";
+            string cmd = $"INSERT INTO ProdutoFaturado (Id, Produto, Quantidade, ForPag) VALUES (0, '{pedidoCombo.Text}', '{int.Parse(preco1.Text)}', '{pagamentoCombo}')";
             var sqlCommand = new SqlCommand(cmd, conn);
             var result = sqlCommand.ExecuteNonQuery();
 
